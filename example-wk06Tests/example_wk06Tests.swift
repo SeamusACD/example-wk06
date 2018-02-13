be0b0c5d8c52db070beb267fddd3c58444c3f1b6
Seamus Donaldson <seamusdonaldson@comcast.net>
2018-02-13 15:47:12 -0500

    Initial Commit

---
+++ example-wk06Tests/example_wk06Tests.swift
@@ -0,0 +1,36 @@
+//
+//  example_wk06Tests.swift
+//  example-wk06Tests
+//
+//  Created by Seamus Donaldson on 2/13/18.
+//  Copyright © 2018 Seamus Donaldson. All rights reserved.
+//
+
+import XCTest
+@testable import example_wk06
+
+class example_wk06Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
